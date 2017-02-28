b2ab12526673f4417e88a4d4b8f63af83f0c8f61
vamsikrishna97 <pvamsikrishna97@gmail.com>
2017-02-28 20:42:25 +0530

    removing medium.R

--- medium1.R
+++
@@ -1,9 +0,0 @@
-library(lubridate)
-library(plyr)
-q<-occ_search(continent = "africa",limit = 12000)
-y<-strptime(q$data$eventDate,"%Y-%m-%dT%H:%M:%S")
-y
-w<-q$data$eventDate
-w
-y<-strptime(w1,"%Y-%m-%d %H:%M:%S")
-y
