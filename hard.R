a320cba6891364052164de660693f207fe6986c5
vamsikrishna97 <pvamsikrishna97@gmail.com>
2017-03-04 02:42:16 +0530

    ADDITIONAL CHANGES

--- hard.R
+++ hard.R
@@ -28,20 +28,11 @@ closetoCentroid<-function(country){
   x<-dataset$data[dataset$data$distCentroid<800000,]## records which are 800,000m i.e 800km from the centroid of the country
  print(x)
  
-#  countryName<-centroids$name[centroids$country==country]
-#  countryName<-countryName[!is.na(countryName)]
-#  countryMap<-get_map(countryName)
-#   ggmap(countryMap) + geom_point(aes(x$decimalLongitude,x$decimalLatitude,col=x$order))
+ ##mapWorld <- borders("world", colour="gray50") # create a layer of borders
+ ##ggplot() + mapWorld + geom_point(aes(x$decimalLongitude,x$decimalLatitude,col=x$order))
  
  ggmap(get_map(country,zoom = 4))+geom_point(data=x, aes(decimalLongitude,decimalLatitude)) + geom_point(data=centroids[centroids$country==country,],aes(longitude,latitude),col="red",size=4)
-  
 
-
-  x<-dataset$data[dataset$data$distCentroid<200000,]## records which are 200,000m i.e 200km from the centroid of the country
- print(x)
- ##mapWorld <- borders("world", colour="gray50") # create a layer of borders
-  ##ggplot() + mapWorld + geom_point(aes(x$decimalLongitude,x$decimalLatitude,col=x$order))
- 
 
  
 
