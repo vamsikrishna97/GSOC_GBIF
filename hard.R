5c875d85267ab861e018e5d4c79fe42b58bd7436
vamsikrishna97 <pvamsikrishna97@gmail.com>
2017-03-04 02:40:03 +0530

    ADDITIONAL CHANGES

--- hard.R
+++ hard.R
@@ -7,13 +7,10 @@
 ##loading packages
 library(rgbif)
 library(geosphere)
-<<<<<<< HEAD
 library(ggmap)
 library(ggplot2)
-=======
 library(ggplot2)
 library(ggmap)
->>>>>>> 50336456343d43eb0c00aaf48b477d846aa2da08
 
 ##read centroids of countries data
 centroids<-read.csv("centroids.csv")
@@ -28,7 +25,6 @@ closetoCentroid<-function(country){
   
   dataset$data$distCentroid<-distCentroid
   
-<<<<<<< HEAD
   x<-dataset$data[dataset$data$distCentroid<800000,]## records which are 800,000m i.e 800km from the centroid of the country
  print(x)
  
@@ -40,13 +36,13 @@ closetoCentroid<-function(country){
  ggmap(get_map(country,zoom = 4))+geom_point(data=x, aes(decimalLongitude,decimalLatitude)) + geom_point(data=centroids[centroids$country==country,],aes(longitude,latitude),col="red",size=4)
   
 
-=======
+
   x<-dataset$data[dataset$data$distCentroid<200000,]## records which are 200,000m i.e 200km from the centroid of the country
  print(x)
  ##mapWorld <- borders("world", colour="gray50") # create a layer of borders
   ##ggplot() + mapWorld + geom_point(aes(x$decimalLongitude,x$decimalLatitude,col=x$order))
  
->>>>>>> 50336456343d43eb0c00aaf48b477d846aa2da08
+
  
 
 }
