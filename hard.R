59a81084e01de6ad5cc88f5596643703c6310a5d
vamsikrishna97 <pvamsikrishna97@gmail.com>
2017-03-06 19:40:03 +0530

    minute channge

--- hard.R
+++ hard.R
@@ -18,7 +18,7 @@ centroids<-read.csv("centroids.csv")
 
 
 closetoCentroid<-function(country){
-  dataset<-occ_search(country = country, hasCoordinate = T,limit = 5000)
+  dataset<-occ_search(country = country, hasCoordinate = T,limit = 1000)
   
   distCentroid<- distm(dataset$data[,c(4,3)],centroids[centroids$country==country,c(3,2)],fun=distHaversine)
   distCentroid<-distCentroid[,!colSums(is.na(distCentroid))>0]
@@ -26,18 +26,20 @@ closetoCentroid<-function(country){
   dataset$data$distCentroid<-distCentroid
   
   x<-dataset$data[dataset$data$distCentroid<800000,]## records which are 800,000m i.e 800km from the centroid of the country
- print(x)
+ 
  
  ##mapWorld <- borders("world", colour="gray50") # create a layer of borders
  ##ggplot() + mapWorld + geom_point(aes(x$decimalLongitude,x$decimalLatitude,col=x$order))
  
- ggmap(get_map(country,zoom = 4))+geom_point(data=x, aes(decimalLongitude,decimalLatitude)) + geom_point(data=centroids[centroids$country==country,],aes(longitude,latitude),col="red",size=4)
-
-
+map<- ggmap(get_map(country,zoom = 4))+
+   geom_point(data=x, aes(decimalLongitude,decimalLatitude)) + 
+   geom_point(data=centroids[centroids$country==country,],aes(longitude,latitude),col="red",size=4)
+print(map)
  
+return(x)
 
 }
 
-closetoCentroid("IN") #####ISO-3166-1
+india<-closetoCentroid("IN") #####ISO-3166-1
 
-closetoCentroid("US")
+US<-closetoCentroid("US")
